f9a234966e576317a6433ae9b8fbe4381739b595
malm7md <malm7md20091@gmail.com>
2024-08-05 19:49:28 +0300

    functions.js

---
+++ functions2.js
@@ -0,0 +1,28 @@
+function isOdd(n) {
+     if( n % 2 === 0 )
+{ return false;
+
+} else  { return true;
+    } 
+
+}
+let result = isOdd(13);
+console.log(result)
+
+
+function squareOrDouble(n) {
+    if(n % 2 == 0) {
+        return n * 2; }
+    else { return n*+2;
+
+    }
+}
+
+let result2 = squareOrDouble(3)
+console.log(result2);
+
+
+
+
+let students = [` "ali" , "Mohammad" , "sss" `]
+console.log(students.length);
